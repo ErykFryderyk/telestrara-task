61155ccd3faba3c3e9ab1ed8232213252b47079d
Gregor <gregor@MacBook-Pro-Admin.local>
2022-08-23 23:25:13 +0200

    create first component

---
+++ src/components/Mytitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Siemano Kolano</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyTitle',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
