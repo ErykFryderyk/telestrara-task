bad6924afeb636d93b54b891c9c1952ccad38a09
Gregor <gregor@MacBook-Pro-Admin.local>
2022-08-24 22:46:04 +0200

    create not found info

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Coś poszło nie tak ;()</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ErrorMessage'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
