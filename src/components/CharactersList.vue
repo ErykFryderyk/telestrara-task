f6b8f7765cbeb914ce0a606b8d63173f13f2c0cb
Gregor <gregor@MacBook-Pro-Admin.local>
2022-08-26 00:10:19 +0200

    create component to show characters

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="characters-list">
+    <h1>Lista bohaterów</h1>
+    <!-- <ul v-for="character in characters" :key="character.id">
+      <li>
+        <h1>character.id</h1>
+      </li>
+    </ul> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharactersList.vue',
+  data() {
+    return {
+      props: [
+        'message',
+      ]
+    }
+  },
+}
+</script>
+
+<style land="scss">
+.character-list {
+  color: black;
+  background-color: black;
+}
+</style>
